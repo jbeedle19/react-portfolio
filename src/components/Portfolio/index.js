bd9191b91166512d54dd1a165278e786f5b0ed09
jbeedle19 <josh.beedle@gmail.com>
2021-03-24 15:30:37 -0400

    finished project/portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,45 +10,8 @@ function Portfolio() {
                     Work
                 </h2>
             </div>
-            <Project />
-            <div className="work-grid-wrapper">
-                <div className="work-grid-container">
-                    <div className="work-grid-item fridge">
-                        <div>
-                            <h2><a className="link" href="https://fridg3-fri3nd.herokuapp.com/" target="_blank" rel="noreferrer">Fridge Friend</a></h2>
-                            <h3><a className="link" href="https://github.com/jbeedle19/fridge-friend" target="_blank" rel="noreferrer">GitHub Repo</a></h3>
-                            <h4>Exrpess.js / Sequelize / Handlebars.js / Tailwind CSS /  Nodemailer</h4>
-                        </div>
-                    </div>
-                    <div className="work-grid-item movers">
-                        <div>
-                            <h2><a className="link" href="https://jbeedle19.github.io/movers-not-shakers/" target="_blank" rel="noreferrer">Movers Not Shakers</a></h2>
-                            <h3><a className="link" href="https://github.com/jbeedle19/movers-not-shakers" target="_blank" rel="noreferrer">GitHub Repo</a></h3>
-                            <h4>jQuery / Bulma CSS / Google Maps API / OpenWeather API</h4>
-                        </div>
-                    </div>
-                    <div className="work-grid-item weather">
-                        <div>
-                            <h2><a className="link" href="https://jbeedle19.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Weather Dashboard</a></h2>
-                            <h3><a className="link" href="https://github.com/jbeedle19/weather-dashboard" target="_blank" rel="noreferrer">GitHub Repo</a></h3>
-                            <h4>jQuery / Bootstrap / Moment.js</h4>
-                        </div>
-                    </div>
-                    <div className="work-grid-item blog">
-                        <div>
-                            <h2><a className="link" href="https://the-t3ch-blog.herokuapp.com/" target="_blank" rel="noreferrer">Tech Blog</a></h2>
-                            <h3><a className="link" href="https://github.com/jbeedle19/tech-blog" target="_blank" rel="noreferrer">GitHub Repo</a></h3>
-                            <h4>Express.js / Sequelize / Handlebars.js</h4>
-                        </div>
-                    </div>
-                    <div className="work-grid-item quiz">
-                        <div>
-                            <h2><a className="link" href="https://jbeedle19.github.io/code-quiz/" target="_blank" rel="noreferrer">Code Quiz</a></h2>
-                            <h3><a className="link" href="https://github.com/jbeedle19/code-quiz" target="_blank" rel="noreferrer">GitHub Repo</a></h3>
-                            <h4>HTML / CSS / JavaScript</h4>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <Project />
             </div>
         </section>
     )
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,6 +5,7 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
 import fridgeImage from '../../assets/images/fridge.svg';
 import moversImage from '../../assets/images/movers-not-shakers.jpg';
@@ -12,7 +13,7 @@ import techImage from '../../assets/images/blog-favicon.png';
 import codeImage from '../../assets/images/quiz-favicon.png';
 import weatherImage from '../../assets/images/weather-favicon.png';
 
-function Project() {
+function Project(props) {
     const useStyles = makeStyles((theme) => ({
         root: {
             display: 'flex',
@@ -22,68 +23,119 @@ function Project() {
             backgroundColor: '#7389ae',
         },
         gridList: {
-            width: 500,
-            height: 450,
+            width: '75%',
+            [theme.breakpoints.down('sm')]: {
+                width: '100%',
+            },
+            [theme.breakpoints.down('sm')]: {
+                width: '100%',
+            },
         },
         title: {
             color: '#7389ae',
         },
+        icon: {
+            color: '#7389ae',
+            '&:hover': {
+                color: '#1d3461',
+            }
+        },
         titleBar: {
             background:
-                'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                'linear-gradient(to top, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.7) 70%, rgba(0,0,0,0.6) 100%)',
         }
     }));
 
     const classes = useStyles();
+
+    const getGridListHeight = () => {
+        if (isWidthUp('md', props.width)) {
+            return 350;
+        }
+    
+        if (isWidthUp('sm', props.width)) {
+            return 275;
+        }
+    
+        if (isWidthUp('xs', props.width)) {
+            return 350;
+        }
+    
+        return 1;
+    }
+
+    const getGridListCols = (project) => {
+        if (isWidthUp('sm', props.width)) {
+            return project.cols;
+        }
+    
+        if (isWidthUp('xs', props.width)) {
+            return 3;
+        }
+    
+        return 1;
+    }
 
     const projectData = [
         {
             img: fridgeImage,
             title: 'Fridge Friend',
             description: 'Express / Sequelize / Handlebars.js / Tailwind / Nodemailer',
+            gitLink: 'https://github.com/jbeedle19/fridge-friend',
+            deployedLink: 'https://fridg3-fri3nd.herokuapp.com/',
             cols: 2,
         },
         {
             img: moversImage,
             title: 'Movers Not Shakers',
             description: 'jQuery / Bulma CSS / Google Maps API / OpenWeather API',
+            gitLink: 'https://github.com/jbeedle19/movers-not-shakers',
+            deployedLink: 'https://jbeedle19.github.io/movers-not-shakers/',
             cols: 1,
         },
         {
             img: techImage,
             title: 'Tech Blog',
             description: 'Express / Sequelize / Handlebars.js',
+            gitLink: 'https://github.com/jbeedle19/tech-blog',
+            deployedLink: 'https://the-t3ch-blog.herokuapp.com/',
             cols: 1,
         },
         {
             img: codeImage,
             title: 'Code Quiz',
             description: 'HTML / CSS / JavaScript',
+            gitLink: 'https://github.com/jbeedle19/code-quiz',
+            deployedLink: 'https://jbeedle19.github.io/code-quiz/',
             cols: 1,
         },
         {
             img: weatherImage,
             title: 'Weather Dashboard',
             description: 'jQuery / Bootstrap / Moment.js',
+            gitLink: 'https://github.com/jbeedle19/weather-dashboard',
+            deployedLink: 'https://jbeedle19.github.io/weather-dashboard/',
             cols: 1,
         }
     ];
 
     return (
         <div className={classes.root}>
-            <GridList cellHeight={160} className={classes.gridList} cols={3}>
+            <GridList cellHeight={getGridListHeight()} className={classes.gridList} cols={3} spacing={10}>
             {projectData.map((project) => (
-                <GridListTile key={project.title} cols={project.cols || 1}>
+                <GridListTile className={classes.gridList} key={project.title} cols={getGridListCols(project)}>
                     <img src={project.img} alt={project.title} />
                     <GridListTileBar 
-                        title={project.title}
+                        title={<a href={project.deployedLink} target="_blank" rel="noreferrer">{project.title}</a>}
+                        subtitle={<span>{project.description}</span>}
                         classes={{
                             root: classes.titleBar,
                             title: classes.title,
+                            subtitle: classes.title,
                         }}
                         actionIcon={
-                            <IconButton aria-label={`GitHub ${project.title}`}>
-                                <GitHubIcon className={classes.title} />
+                            <IconButton aria-label={`${project.title} GitHub`} onClick={() => window.open(project.gitLink, "_blank")}>
+                                <GitHubIcon className={classes.icon} />
                             </IconButton>
                         }
                     />
@@ -94,4 +146,4 @@ function Project() {
     );
 }
 
-export default Project;
+export default withWidth()(Project);
